95c237dd16c95a20bad69296f67a450ddef1aa2e
Lifan Huang <lifan_huang@outlook.com>
2023-02-20 22:05:18 +0800

    style: change magic number to variables

--- utils/LdpResultCalculator.ts
+++ utils/LdpResultCalculator.ts
@@ -28,7 +28,7 @@ export default function calcLdp(inputValues: InputValues): ResultAndFees {
   const quantity = inputValues.quantity;
   const exchangeRate = inputValues.exchangeRate;
   const clearancePrice = inputValues.clearancePrice;
-  const taxRate = inputValues.taxRate;
+  const taxRatePercent = inputValues.taxRate;
   const trucking = inputValues.trucking;
   const totalUnitsVolume = inputValues.volume;
   const estimatedFeePerUnit = inputValues.estimatedFeePerUnit;
@@ -47,56 +47,19 @@ export default function calcLdp(inputValues: InputValues): ResultAndFees {
 
   // 计算关税（体积无关）
   const totalClearance: string = bigDecimal.multiply(quantity, clearancePrice);
+  // H/M 港口维护费
+  const HMPortMaintainingRate: string = '0.001250';
+  // M/P 货物处理费
+  const MPCargoRate: string = '0.003464';
   const duty = bigDecimal.add(
     bigDecimal.multiply(
       totalClearance,
-      bigDecimal.divide(taxRate, 100, 3)
+      bigDecimal.divide(taxRatePercent, 100, 3)
     ),
     bigDecimal.add(
-      bigDecimal.multiply('0.001250', totalClearance),
-      bigDecimal.multiply('0.003464', totalClearance))
+      bigDecimal.multiply(HMPortMaintainingRate, totalClearance),
+      bigDecimal.multiply(MPCargoRate, totalClearance))
   );
-
-  // // 散柜部分
-  // if (inputValues.type === 'bulk') {
-  //   landShippingFee = bigDecimal.add(
-  //     bigDecimal.divide(100, exchangeRate, 2),
-  //     bigDecimal.divide(100, exchangeRate, 2)
-  //   );
-  //   oceanShippingFee = bigDecimal.add(
-  //     bigDecimal.multiply(volume, estimatedFeePerUnit),
-  //     30
-  //   );
-  //   destinationPortFee = bigDecimal.add(
-  //     bigDecimal.add(
-  //       bigDecimal.add(85, 65),
-  //       trucking
-  //     ),
-  //     bigDecimal.add(
-  //       bigDecimal.multiply(volume, 3),
-  //       bigDecimal.add(
-  //         bigDecimal.multiply(volume, 10),
-  //         bigDecimal.multiply(volume, 35))
-  //     )
-  //   );
-  // } else {
-  //   // whole 整柜
-  //   landShippingFee = bigDecimal.divide(
-  //     480 + 450 + 1230 + 50 + 100 + 100 + 800 + 1000,
-  //     exchangeRate,
-  //     2
-  //   );
-  //   oceanShippingFee = bigDecimal.add(
-  //     estimatedFeePerContainer, 30
-  //   );
-  //   destinationPortFee = bigDecimal.add(
-  //     bigDecimal.add(
-  //       bigDecimal.add(85, 65),
-  //       trucking
-  //     ),
-  //     35 + 134 + 200 + 75 + 300 + 150
-  //   )
-  // }
 
   const unitBasedFees: VolumeBasedFees = calcUnitBasedFees(exchangeRate, trucking, estimatedFeePerUnit, volumeWithoutContainer)
   const containerBasedFees: VolumeBasedFees = calcContainerBasedFees(exchangeRate, trucking, estimatedFeePerContainer, containerNum)
@@ -120,30 +83,50 @@ export default function calcLdp(inputValues: InputValues): ResultAndFees {
   };
 }
 
-function calcUnitBasedFees(exchangeRate: string, trucking: string, estimatedFeePerUnit: string, volume: string): VolumeBasedFees {
+function calcUnitBasedFees(exchangeRate: string,
+                           trucking: string,
+                           estimatedFeePerUnit: string,
+                           volume: string): VolumeBasedFees {
+  // 内陆费
+  // 报关费 RMB
+  const clearanceFeeRMB: number = 100;
+  // 舱单费 RMB
+  const chamberOrderFeeRMB: number = 100;
   const landShippingFee = bigDecimal.add(
-    bigDecimal.divide(100, exchangeRate, 2),
-    bigDecimal.divide(100, exchangeRate, 2)
+    bigDecimal.divide(clearanceFeeRMB, exchangeRate, 2),
+    bigDecimal.divide(chamberOrderFeeRMB, exchangeRate, 2)
   );
+
+  // 海运费
+  // AMS
+  const amsFee: number = 30;
   const oceanShippingFee = bigDecimal.add(
     bigDecimal.multiply(volume, estimatedFeePerUnit),
-    30
+    amsFee
   );
-  // ENTRY 85
-  // H/D CHARGE 65
-  // CHASSIS CHARGE 3
-  // pallect 10
-  // FOLK LIFT CHARGE 49
+
+  // fee per volume
+  // entry
+  const entryFeePerVolume: number = 85;
+  // H/D charge
+  const hdChargeFeePerVolume: number = 65;
+  // chassis charge
+  const chassisChargeFeePerVolume: number = 3;
+  // pallet
+  const palletFeePerVolume: number = 10;
+  // folk lift charge
+  const folkLiftChargeFeePerVolume: number = 49;
+  // 目的港费用
   const destinationPortFee = bigDecimal.add(
     bigDecimal.add(
-      bigDecimal.add(85, 65),
+      bigDecimal.add(entryFeePerVolume, hdChargeFeePerVolume),
       trucking
     ),
     bigDecimal.add(
-      bigDecimal.multiply(volume, 3),
+      bigDecimal.multiply(volume, chassisChargeFeePerVolume),
       bigDecimal.add(
-        bigDecimal.multiply(volume, 10),
-        bigDecimal.multiply(volume, 49))
+        bigDecimal.multiply(volume, palletFeePerVolume),
+        bigDecimal.multiply(volume, folkLiftChargeFeePerVolume))
     )
   );
   return {
@@ -153,21 +136,65 @@ function calcUnitBasedFees(exchangeRate: string, trucking: string, estimatedFeeP
   };
 }
 
-function calcContainerBasedFees(exchangeRate: string, trucking: string, estimatedFeePerContainer: string, containerNum: string): VolumeBasedFees {
+function calcContainerBasedFees(exchangeRate: string,
+                                trucking: string,
+                                estimatedFeePerContainer: string,
+                                containerNum: string): VolumeBasedFees {
+  // domestic part
+  // 订舱费
+  const chamberReservationFeeRMB: number = 480;
+  // 单证费
+  const singleCertFeeRMB: number = 450;
+  // THC
+  const THCFeeRMB: number = 1230;
+  // 铅封费
+  const sealFeeRMB: number = 50;
+  // 报关费
+  const clearanceFeeRMB: number = 100;
+  // 舱单费
+  const chamberOrderFeeRMB: number = 100;
+  // 内装
+  const innerFeeRMB: number = 800;
+  // 洋山港
+  const yangShanFeeRMB: number = 1000;
   const landShippingFee = bigDecimal.divide(
-    480 + 450 + 1230 + 50 + 100 + 100 + 800 + 1000,
+    chamberReservationFeeRMB + singleCertFeeRMB + THCFeeRMB + sealFeeRMB + clearanceFeeRMB +
+    chamberOrderFeeRMB + innerFeeRMB + yangShanFeeRMB,
     exchangeRate,
     2
   );
+
+  // transit part
+  const amsFee: number = 30;
   const oceanShippingFee = bigDecimal.add(
-    estimatedFeePerContainer, 30
+    estimatedFeePerContainer, amsFee
   );
+
+  // foreign part
+  // entry fee
+  const entryFee: number = 85;
+  // H/D charge fee
+  const hdChargeFee: number = 65;
+  // ISF filing fee
+  const isfFilingFee: number = 35;
+  // ISF bond fee
+  const isfBondFee: number = 0;
+  // pier pass fee
+  const pierPassFee: number = 134;
+  // chassis charge fee
+  const chassisChargeFee: number = 200;
+  // clean truck fee
+  const cleanTruckFee: number = 75;
+  // pre-pull fee
+  const prePullFee: number = 300;
+  // congestion fee
+  const congestionFee: number = 150;
   const destinationPortFee = bigDecimal.add(
     bigDecimal.add(
-      bigDecimal.add(85, 65),
+      bigDecimal.add(entryFee, hdChargeFee),
       trucking
     ),
-    35 + 134 + 200 + 75 + 300 + 150
+    isfFilingFee + isfBondFee + pierPassFee + chassisChargeFee + cleanTruckFee + prePullFee + congestionFee
   );
   return {
     landShippingFee: bigDecimal.multiply(landShippingFee, containerNum),
