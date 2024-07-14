f98e5b06de5299e6ef4fb550be985a17b172f67f
Lifan Huang <lifan_huang@outlook.com>
2024-07-14 21:30:00 +0800

    feat: update to next.js 14

--- next.config.js
+++ next.config.js
@@ -2,4 +2,5 @@
 module.exports = {
   reactStrictMode: true,
   output: 'standalone',
+  transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ],
 }
